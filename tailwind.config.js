6dad890632f930befb6fc7873072d94aaf06faa5
PRAKASH RAJA <119003888+prakashraja-0723@users.noreply.github.com>
2023-12-27 12:22:46 +0530

    Update styles and Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        orange: "hsl(26, 100%, 55%)",
+        "pale-orange": "hsl(25,100%,94%)",
+        "very-dark-blue": "hsl(220, 13%, 13%)",
+        "dark-grayish-blue": "hsl(219, 9%, 45%)",
+        "grayish-blue": "hsl(220, 14%, 75%)",
+        "light-grayish-blue": "hsl(223, 64%, 98%)",
+        lightbox: "hsla(0,0%,0%,0.75)",
+      },
+      textColor: {
+        orange: "hsl(26, 100%, 55%)",
+        "pale-orange": "hsl(25,100%,94%)",
+        "very-dark-blue": "hsl(220, 13%, 13%)",
+        "dark-grayish-blue": "hsl(219, 9%, 45%)",
+        "grayish-blue": "hsl(220, 14%, 75%)",
+        "light-grayish-blue": "hsl(223, 64%, 98%)",
+      },
+      borderColor : {
+        orange: "hsl(26, 100%, 55%)",
+        "pale-orange": "hsl(25,100%,94%)",
+        "very-dark-blue": "hsl(220, 13%, 13%)",
+        "dark-grayish-blue": "hsl(219, 9%, 45%)",
+        "grayish-blue": "hsl(220, 14%, 75%)",
+        "light-grayish-blue": "hsl(223, 64%, 98%)",
+      },
+      fill:{
+        orange: "hsl(26, 100%, 55%)",
+        "pale-orange": "hsl(25,100%,94%)",
+        "very-dark-blue": "hsl(220, 13%, 13%)",
+        "dark-grayish-blue": "hsl(219, 9%, 45%)",
+        "grayish-blue": "hsl(220, 14%, 75%)",
+        "light-grayish-blue": "hsl(223, 64%, 98%)",
+      }
+    },
   },
   plugins: [],
-}
-
+};
