a57afb5866bbc618659c6965de7c046450ca67d3
PRAKASH RAJA <119003888+prakashraja-0723@users.noreply.github.com>
2023-12-27 13:35:02 +0530

    Refactor AddCart and CartCard components

--- src/Components/AddCart.jsx
+++ src/Components/AddCart.jsx
@@ -12,11 +12,11 @@ const AddCart = ({currentQuantity,addQuantity,removeQuantity,addItemToCart}) => 
           className={`h-14  bg-light-grayish-blue text-black font-bold flex items-center justify-between w-full rounded-[12px] md:w-5/12 md:rounded-[6px] lg:rounded-xl`}
         >
           <span onClick={removeQuantity} className={`px-6 h-full flex items-center`}>
-            <svg width="12" height="4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M11.357 3.332A.641.641 0 0 0 12 2.69V.643A.641.641 0 0 0 11.357 0H.643A.641.641 0 0 0 0 .643v2.046c0 .357.287.643.643.643h10.714Z" id="a" /></defs><use fill="#FF7E1B" fill-rule="nonzero" xlink:href="#a" /></svg>
+            <svg width="12" height="4" xmlns="http://www.w3.org/2000/svg"><path d="M11.357 3.332A.641.641 0 0 0 12 2.69V.643A.641.641 0 0 0 11.357 0H.643A.641.641 0 0 0 0 .643v2.046c0 .357.287.643.643.643h10.714Z" id="a" fill="#FF7E1B"/></svg>
           </span>
           <span>{currentQuantity}</span>
           <span onClick={addQuantity} className={`px-6 h-full flex items-center`}>
-            <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M12 7.023V4.977a.641.641 0 0 0-.643-.643h-3.69V.643A.641.641 0 0 0 7.022 0H4.977a.641.641 0 0 0-.643.643v3.69H.643A.641.641 0 0 0 0 4.978v2.046c0 .356.287.643.643.643h3.69v3.691c0 .356.288.643.644.643h2.046a.641.641 0 0 0 .643-.643v-3.69h3.691A.641.641 0 0 0 12 7.022Z" id="b" /></defs><use fill="#FF7E1B" fill-rule="nonzero" xlink:href="#b" /></svg>
+            <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M12 7.023V4.977a.641.641 0 0 0-.643-.643h-3.69V.643A.641.641 0 0 0 7.022 0H4.977a.641.641 0 0 0-.643.643v3.69H.643A.641.641 0 0 0 0 4.978v2.046c0 .356.287.643.643.643h3.69v3.691c0 .356.288.643.644.643h2.046a.641.641 0 0 0 .643-.643v-3.69h3.691A.641.641 0 0 0 12 7.022Z" id="b" fill="#FF7E1B" /></svg>
           </span>
         </button>
         <button
